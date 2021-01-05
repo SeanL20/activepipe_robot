19de06ea4282a8ef783c03e873e305f9000698ed
Sean Lim <seanl@presentplate.com>
2021-01-05 16:25:32 +1000

    Title: Add Keywords For The Initialization Of The Robot & Splitting The Hit Board Function To X & Y Axis.
    Status: Complete.

--- model/robot.rb
+++ model/robot.rb
@@ -2,17 +2,21 @@ class Robot
 	attr_accessor :robot_name, :x_posit, :y_posit, :facing
   attr_reader :board
 
-	def initialize(robot_name, x_position, y_position, facing, board)
+	def initialize(robot_name:, x_position:, y_position:, facing:, board:)
 		@robot_name = robot_name
 		@x_posit = x_position
 		@y_posit = y_position
 		@facing = facing
 		@board = board
 
-		@board << self
+		@board.robots << self
 	end
 
-	def hit_border?
-		x_position == 0 || x_position == 6 || y_position == 0 || y_position == 6
+	def x_hit_border?
+		x_posit == 0 || x_posit == 6
+	end
+
+	def y_hit_border?
+		y_posit == 0 || y_posit == 6
 	end
 end
