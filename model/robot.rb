afad449c22c4193938350f587bcd511000246949
Sean Lim <seanl@presentplate.com>
2021-01-06 10:16:48 +1000

    Title: Add Function To Check If The Robot Would Hit The Border Or Move To The Edge.
    Status: Complete.

--- model/robot.rb
+++ model/robot.rb
@@ -5,21 +5,51 @@ class Robot
   # Initialize a robot class & link to the board class so that the robot can be display as an array.
 	def initialize(robot_name:, x_position:, y_position:, facing:, board:)
 		@robot_name = robot_name
-		@x_posit = x_position
-		@y_posit = y_position
+		@x_posit = x_position.to_i
+		@y_posit = y_position.to_i
 		@facing = facing
 		@board = board
 
 		@board.robots << self
 	end
 
-	# Get the boolean if the robot in the position that it would hit the border if it moves in the x axis.
-	def x_hit_border?
-		x_posit == 0 || x_posit == 6
+	# Get the boolean if the robot in the position that it would hit the north border if it moves in the y axis.
+	def y_hit_north_border?
+		y_posit == 6
 	end
 
-	# Get the boolean if the robot in the position that it would hit the border if it moves in the y axis.
-	def y_hit_border?
-		y_posit == 0 || y_posit == 6
+	# Get the boolean if the robot in the position that it would hit the south border if it moves in the y axis.
+	def y_hit_south_border?
+		y_posit == 0
+	end
+
+	# Get the boolean if the robot would move to the north edge (y coord of 6) if it moves in the y axis.
+	def y_move_to_north_edge?
+		(y_posit + 1) != 6
+	end
+
+	# Get the boolean if the robot would move to the south edge (y coord of 0) if it moves in the y axis.
+	def y_move_to_south_edge?
+		(y_posit - 1) != 0
+	end
+
+	# Get the boolean if the robot in the position that it would hit the east border if it moves in the x axis.
+	def x_hit_east_border?
+		x_posit == 6
+	end
+
+	# Get the boolean if the robot in the position that it would hit the west border if it moves in the x axis.
+	def x_hit_west_border?
+		x_posit == 0
+	end
+
+	# Get the boolean if the robot would move to the east edge (x coord of 6) if it moves in the x axis.
+	def x_move_to_east_edge?
+		(x_posit + 1) != 6
+	end
+
+	# Get the boolean if the robot would move to the west edge (x coord of 0) if it moves in the x axis.
+	def x_move_to_west_edge?
+		(x_posit - 1) != 0
 	end
 end
